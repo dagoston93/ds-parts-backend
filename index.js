9e4e3f8591a672a17fa1fce01550f73c06ef2a72
Agoston Diamont <dagoston93@gmail.com>
2024-11-07 23:27:14 +0100

    Express added, skeleton of service created.

---
+++ index.js
@@ -0,0 +1,85 @@
+const express = require("express");
+const Joi = require("joi");
+
+const app = express();
+app.use(express.json());
+
+////// Temp data
+const parts = [
+    { id: 0, name: "Resistor"},
+    { id: 1, name: "Inductor"},
+    { id: 2, name: "Capacitor"},
+]
+
+app.get("/", (req, res) => res.send("Hello World."));
+
+app.get("/api/parts", (req, res) => res.send(parts));
+
+app.get("/api/parts/:id", (req, res) => {
+    const part = parts.find(p => p.id == parseInt(req.params.id));
+    if(!part) {
+        res.status(404).send("Part with given ID not found.");
+    }else{
+        res.send(part);
+    }
+});
+
+app.post("/api/parts", (req, res) => {
+    const {error} = validatePart(req.body);
+    if(error) {
+        res.status(400).send("Bad Request!\n" + error.details[0].message);
+        return;
+    }
+
+    const part = {
+        id: parts.length,
+        name: req.body.name
+    }
+
+    parts.push(part);
+    res.send(part);
+});
+
+app.put("/api/parts/:id", (req, res) => {
+    const part = parts.find(p => p.id == parseInt(req.params.id));
+
+    if(!part) {
+        res.status(404).send("Part with given ID not found.");
+        return;
+    }
+
+    const {error} = validatePart(req.body);
+    if(error) {
+        res.status(400).send("Bad Request!\n" + error.details[0].message);
+        return;
+    }
+
+    part.name = req.body.name;
+    res.send(part);
+
+});
+
+app.delete("/api/parts/:id", (req, res) => {
+    const part = parts.find(p => p.id == parseInt(req.params.id));
+
+    if(!part) {
+        res.status(404).send("Part with given ID not found.");
+        return;
+    }
+
+    const index = parts.indexOf(part);
+    parts.splice(index, 1);
+
+    res.send(part);
+});
+
+const port = process.env.DS_PARTS_BE_PORT || 3000;
+app.listen(port, () => console.log(`Listening on port ${port}...`));
+
+function validatePart(part) {
+    const schema = Joi.object({
+        name: Joi.string().min(3).required()
+    });
+
+    return schema.validate(part);
+}
