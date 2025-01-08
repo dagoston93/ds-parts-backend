6d9e8284c486fec42e5e429b647fb89cecd5a766
Agoston Diamont <dagoston93@gmail.com>
2025-01-08 23:51:00 +0100

    Custom fields added to category.

--- models/category.js
+++ models/category.js
@@ -3,6 +3,28 @@ const Joi = require("joi");
 const lodash = require("lodash");
 const autopopulate = require("mongoose-autopopulate");
 
+const customFieldSchema = new mongoose.Schema({
+    id: {
+        type: String,
+        required: true
+    },
+    name: {
+        type: String,
+        trim: true,
+        required: true,
+        minlength: 2,
+        maxlength: 150
+    },
+    type: {
+        type: String,
+        required: true
+    },
+    required: {
+        type: Boolean,
+        required: true
+    }
+});
+
 const categorySchema = new mongoose.Schema({
     name: { 
         type: String,
@@ -19,7 +41,8 @@ const categorySchema = new mongoose.Schema({
         type: mongoose.Schema.Types.ObjectId,
         ref: "Category",
         autopopulate: { select: "name" }
-    }
+    },
+    customFields:  [customFieldSchema]
 });
 
 categorySchema.plugin(autopopulate);
@@ -30,14 +53,15 @@ function validate(category) {
     const schema = Joi.object({
         name: Joi.string().min(2).max(255).required(),
         createdBy: Joi.objectId(),
-        parent: Joi.objectId()
+        parent: Joi.objectId(),
+        customFields: Joi.array().items(Joi.object())
     });
 
     return schema.validate(category);
 }
 
 function pickProperties(obj) {
-    return lodash.pick(obj, ["name", "parent"]);
+    return lodash.pick(obj, ["name", "parent", "customFields"]);
 }
 
 async function exists(id) {
